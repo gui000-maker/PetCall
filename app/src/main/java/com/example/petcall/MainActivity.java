c1ab2b4410b117769d358f876ffbb5c3599e97a8
gui <gui151208@gmail.com>
2025-11-05 21:47:35 -0300

    new features

--- app/src/main/java/com/example/petcall/MainActivity.java
+++ app/src/main/java/com/example/petcall/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.petcall;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,20 +13,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-    public void Next(View view) {
+    public void next(View view) {
         Intent intent = new Intent(this, Function.class);
         startActivity(intent);
     }
